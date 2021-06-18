ac78673712ed31e9e68e6a7c0f77c5d7f1f52901
Josh Batt <josh.batt@gmail.com>
2021-06-18 11:11:49 +0100

    Josh changes v1
    Trying to isolate the 2 relevant lanes

--- test.py
+++ test.py
@@ -41,7 +41,22 @@ cannyed = cv2.Canny(dilated, 100, 200)
 
 contours, h = cv2.findContours(cannyed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-contours_drawn = cv2.drawContours(cropped_colour, contours, -1, GREEN, 2, cv2.LINE_8, h)
+#contours_drawn = cv2.drawContours(cropped_colour, contours, -1, GREEN, 2, cv2.LINE_8, h)
+
+
+contours_sorted = sorted(contours, key=cv2.contourArea, reverse=True)
+
+#print(len(contours_sorted))
+
+contours_drawn = cv2.drawContours(cropped_colour, contours_sorted[:8], -1, GREEN, 2, cv2.LINE_8)
+
+print(contours_sorted[0][:,0,1].shape)
+
+rows,cols = (69,69)
+[vx,vy,x,y] = cv2.fitLine(contours_sorted[0], cv2.DIST_L2,0,0.01,0.01)
+lefty = int((-x*vy/vx) + y)
+righty = int(((cols-x)*vy/vx)+y)
+test = cv2.line(contours_sorted[0],(cols-1,righty),(0,lefty),(0,255,0),2)
 
 # lines = cv2.HoughLines(cannyed, 1, np.pi/180, cannyed.shape[0]-1)
 #
@@ -59,7 +74,7 @@ contours_drawn = cv2.drawContours(cropped_colour, contours, -1, GREEN, 2, cv2.LI
 #         cv2.line(test_image_copy, (x1, y1+height), (x2, y2+height), GREEN, 2)
 
 
-cv2.imshow('test', contours_drawn)
+cv2.imshow('test', test)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
